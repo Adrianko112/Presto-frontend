de4284e58a865cf4fa36f692522167f8bf11a27b
Adriano Germinal <adrianogerminal@gmail.com>
2026-03-26 14:52:15 +0100

    completato filtro categorie

--- annunci.js
+++ annunci.js
@@ -30,24 +30,38 @@ function truncateString(str) {
     return str;
 }
 
+function cardCreate(array) {
+cardWrapper.innerHTML = '';
 
-
-
-function cardCreate() {
-
-    data.forEach((annuncio) => {
+    array.forEach((annuncio, i) => {
         let div = document.createElement('div');
         div.classList.add('card-custom');
-        div.innerHTML = `<p class="h2">${truncateString(annuncio.name)}</p>
+        div.innerHTML = `<img src="https://picsum.photos/${300+i}" class="card-img-top img-fluid img-card" alt="${annuncio.name}">
+                         <p class="h2" title="${annuncio.name}">${truncateString(annuncio.name)}</p>
                          <p class="h4">${annuncio.category}</p>
                          <p class="lead">${annuncio.price} €</p>`; 
         cardWrapper.appendChild(div);
     }); 
 }
 
-cardCreate();
+cardCreate(data);
 
+function filterCards(category) {
+    if (category!='all') {
+        let filteredData = data.filter((annuncio) => annuncio.category === category);
+        cardCreate(filteredData);
+    } else {
+        cardCreate(data);
+    }
 
+}
 
+let radioButtons = document.querySelectorAll('.form-check-input');
+
+radioButtons.forEach((Button) => {
+    Button.addEventListener('click', () => {
+      filterCards(Button.id);
+    }); 
+}); 
 });
 
